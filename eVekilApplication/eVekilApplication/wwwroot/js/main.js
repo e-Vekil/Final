c78e045af06121399f86c983862628f758a0e285
Murad Ibrahimxanli <Ibrahimxanlimurad@hotmail.com>
2018-10-24 04:33:16 +0400

    EditingBordersAdded(COMMENTS)

---
+++ eVekilApplication/eVekilApplication/wwwroot/js/main.js
@@ -0,0 +1,30 @@
+﻿var box = $('#navPrimary');
+
+$('.navToggle').on('click', function () {
+
+    if (box.hasClass('hidden')) {
+
+        box.removeClass('hidden');
+        setTimeout(function () {
+            box.removeClass('navHidden');
+        }, 20);
+
+    } else {
+
+        box.addClass('navHidden');
+
+        box.one('transitionend', function (e) {
+
+            box.addClass('hidden');
+
+        });
+
+    }
+
+});
+
+////////////////MURAD END/////////////////
+
+////////////////TERLAN START/////////////////
+
+////////////////TERLAN END/////////////////
