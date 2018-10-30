3c76604af8722ec0f9e467b5a65d5ee4835995b9
Murad Ibrahimxanli <Ibrahimxanlimurad@hotmail.com>
2018-10-30 09:38:15 +0400

    DynamicURLChangeAdded

--- eVekilApplication/eVekilApplication/wwwroot/js/main.js
+++ eVekilApplication/eVekilApplication/wwwroot/js/main.js
@@ -188,6 +188,11 @@ $(window).scroll(function () {
                 activeIcon.classList.remove("active");
                 activeIcon.classList.remove("active");
                 document.querySelector(".slider." + icon.id).classList.add("active");
+                //window.location.hash = icon.id;
+                //location.replace('https://localhost:44343/#' + icon.id);
+                //parent.location.hash = icon.id;
+                history.replaceState('', 'https://localhost:44343/#main2', icon.id);
+                //setTimeout(function (e) { e.preventDefault();window.location.hash = icon.id; return false }, 700);
             }
             break;
         }
