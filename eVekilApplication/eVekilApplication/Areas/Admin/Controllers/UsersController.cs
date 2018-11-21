5581ea2d7ce987d2a34e3c1bd7dbdd98e04e7426
Murad Ibrahimxanli <Ibrahimxanlimurad@hotmail.com>
2018-11-21 05:52:52 +0400

    AdminPanelUsersAdded(NOT FINISHED)

---
+++ eVekilApplication/eVekilApplication/Areas/Admin/Controllers/UsersController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using eVekilApplication.Data;
+using eVekilApplication.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace eVekilApplication.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Authorize(Roles = "Admin")]
+    public class UsersController : Controller
+    {
+        private readonly EvekilDb _db;
+        public UsersController(EvekilDb db)
+        {
+            _db = db;
+        }
+
+        public async Task<IActionResult> List()
+        {
+            List<User> users;
+            using (_db)
+            {
+                users = await _db.Users.OrderByDescending(x => x.RegisterDate).ToListAsync();
+            }
+            return View(users);
+        }
+    }
+}
---
+++ eVekilApplication/eVekilApplication/Areas/Admin/Models/ViewModels/UserViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eVekilApplication.Areas.Admin.Models.ViewModels
+{
+    public class UserViewModel
+    {
+    }
+}
