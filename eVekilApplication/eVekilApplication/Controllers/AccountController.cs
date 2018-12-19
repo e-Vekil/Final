07935d54c8ed0068156e0dbe251ac86446bfe4f3
terlan427 <terlan427@gmail.com>
2018-12-19 15:58:01 +0400

    EmailConfirmationFixedAdded

--- eVekilApplication/eVekilApplication/Controllers/AccountController.cs
+++ eVekilApplication/eVekilApplication/Controllers/AccountController.cs
@@ -90,6 +90,8 @@ namespace eVekilApplication.Controllers
                                     HttpContext.Session.SetString("id", user.Id);
                                     HttpContext.Session.SetString("name", user.UserName);
                                     HttpContext.Session.SetString("isLoged", "true");
+                                    HttpContext.Session.Remove("emailConfirmed");
+                                    HttpContext.Session.Remove("isSended");
                                     return RedirectToAction("Home", "Account", new { area = "" });
                                 }
                             }
@@ -133,7 +135,8 @@ namespace eVekilApplication.Controllers
                         await service.SendMailAsync(user.Email, "E-VAKIL.AZ TESDIQ", acceptMessage);
                         //Email Confirm End
 
-                        HttpContext.Session.SetString("isRegisterValid", "true");
+                        HttpContext.Session.SetString("isSended", "true");
+                        HttpContext.Session.SetString("email", user.Email);
                         await _userManager.AddToRoleAsync(user, "user");
                         var message = $@"{user.Name} {user.Surname} {user.RegisterDate} tarixində saytdan qeydiyyatdan keçmişdir.";
                         await service.SendMailAsync("ibrahimxanlimurad@hotmail.com", "USER REGISTER", message);
@@ -171,6 +174,8 @@ namespace eVekilApplication.Controllers
             IdentityResult result =await _userManager.ConfirmEmailAsync(user, token);
             if (result.Succeeded)
             {
+                HttpContext.Session.Clear();
+                HttpContext.Session.SetString("emailConfirmed","true");
                 return RedirectToAction("Home", "Account");
             }
             else
@@ -363,7 +368,7 @@ namespace eVekilApplication.Controllers
                 }
 
                 //List<PurchasedDocument> AllPD = await _db.PurchasedDocuments.Where(x => x.User == user).ToListAsync();
-
+                
                 //if (AllPD.Count == SP.Count)
                 //{
                 //}
